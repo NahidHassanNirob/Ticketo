84a562a0c64a53ebd25cf676d25962281844a929
nirob <nhnirob362@gmail.com>
2026-07-18 22:54:21 +0600

    implement pathod for update data

--- src/lib/api/organization/action.js
+++ src/lib/api/organization/action.js
@@ -6,3 +6,9 @@ export const addOrganization=async(data)=>{
     const res= await serverMutation("api/organizations","POST",data)
     return res;
 }
+export const updateOrganization=async(data,id)=>{
+    console.log(data,id);
+    // return;
+    const res= await serverMutation(`api/organization/${id}`,"PATCH",data)
+    return res;
+}
--- src/lib/api/organization/data.js
+++ src/lib/api/organization/data.js
@@ -2,6 +2,5 @@ import { serverFetch } from "../server";
 
 export const getOrganizationData = async (email) => {
   const res = await serverFetch(`api/organization/${email}`);
-  console.log(res,"result");
   return res;
 };
